87e0e55139a9a189264facf281282222f8476e61
Ishwarya <moriyaishu2244@gmail.com>
2026-03-17 12:03:30 +0530

    Implement array insertion examples in array.py
    Added examples demonstrating array insertion in Python.

---
+++ dsa/basics/array.py
@@ -0,0 +1,26 @@
+##🔹 What is Array Insertion?
+##
+##Array insertion means adding a new element at:
+##1️⃣ End
+##2️⃣ Beginning
+##3️⃣ Specific index
+##
+##append=end
+##insert=beginning
+
+
+#1.Q1️⃣ Insert 50 at end
+arr=[5,10,15]
+arr.append(50)
+print(arr)
+
+#2.Q2️⃣ Insert 1 at beginning
+arr=[3,6,9]
+arr.insert(0,1)
+print(arr)
+
+
+#Q3️⃣ Insert 100 at index 2
+arr=[10,20,30,40]
+arr.insert(2,100)
+print(arr)
