6dd26997e87612163c4869e3b9deb77ad0d05a26
Ishwarya <moriyaishu2244@gmail.com>
2026-04-07 20:36:19 +0530

    Add files via upload

---
+++ dsa/basics/1st week practice - session.py
@@ -0,0 +1,82 @@
+##1. what is an array ?
+##
+##array is collection of elements
+##
+##2.why do we use arrays?
+##
+##*to store elements
+##*easily can find
+##*Store multiple values
+##*Access elements using index
+##
+##3.what is linear search?
+##
+##linear search is search elements in array one by one
+##
+##4.on which type of array does linear search work ?
+##
+##unsorted array
+##also works on sorted array but not required.
+##
+##
+##5. what is the time complexity of linear search?
+##
+##O(n)
+##
+##6. why is linear search slow for large arrays?
+##
+##bcz it checks one by one in array
+##
+##
+##7. what is array traversal?
+##
+##visiting one by one elements in array
+##
+##8. write code to print all elements of this array:
+##
+##arr=[4,8,2,9]
+##for x in arr:
+##    print(x)
+##
+##
+##9.how can we insert an element into an array in python?
+##
+##by insert
+##
+##10. arr=[10,20,30]
+##arr.insert(1,50)
+##print(arr)
+##
+##
+##11.how do we delete an element by index in python?
+##
+##by using pop
+##
+##12. how do we delete an element by value in python?
+##
+##by using pop
+
+
+##13. which search works faster on large sorted arrays?
+##
+##binary search
+##
+##14. why?
+##
+##bcz , binary search works on sorted arrays so easily can find it . and also it checks first mid element so it's faster.
+##
+##Binary search is faster because it repeatedly divides the search space in half by checking the middle element.
+##
+##15. reverse method.
+##
+##arr=[1,2,3,4]
+##print(arr[::-1])
+
+
+
+
+
+
+
+
+
