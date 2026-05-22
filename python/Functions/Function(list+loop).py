8e2b4b812dd5e32ae9662cebe494929f7e892a6b
Ishwarya <moriyaishu2244@gmail.com>
2026-05-22 18:00:06 +0530

    Add files via upload

---
+++ python/Functions/Function(list+loop).py
@@ -0,0 +1,29 @@
+##Question 1
+##
+##Create a list of 5 numbers and print the entire list.
+
+num=[1,2,3,4,5]
+print(num)
+
+##Question 2
+##Create a list of 3 fruits and print the second fruit.
+
+fruits=["apple","orange","mango"]
+print(fruits[1])
+
+
+##Question 3
+##Using a loop, print all elements from this list:
+
+colors = ["red", "blue", "green"]
+for x in colors:
+    print(x)
+
+##4.Create a list of marks:
+
+marks=[78, 90, 67, 85]
+for x in marks:
+    if x>35:
+        print("Pass",x)
+    else:
+        print("Fail",x)
