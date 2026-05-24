ac31c60ed5098e5783cb338639b7b04991108e02
Ishwarya <moriyaishu2244@gmail.com>
2026-05-24 15:16:20 +0530

    Add files via upload

---
+++ python/Functions/functions 4.py
@@ -0,0 +1,111 @@
+##Strings + User Input + String Methods
+##strings are used everywhere:
+##
+##chatbots
+##login systems
+##APIs
+##AI/NLP
+##websites
+##forms
+##search systems
+##
+##Almost every real application handles text.
+##Part 1 — What is a String?
+##
+##A string is text/data written inside quotes.
+##
+##Example
+##name = "Ishu"
+##
+##Part 2 — Accessing Characters
+##
+##Strings also use indexing.
+##Part 3 — String Methods
+##Methods are built-in functions for strings.
+##1. upper()
+##Converts to uppercase.
+##2. lower()
+##Converts to lowercase.
+##3. capitalize()
+##Makes first letter capital.
+##4. len()
+##Finds string length.
+##Part 4 — User Input
+##
+##Programs can take input from users.
+##
+##Part 5 — Combining Strings
+##first = "Ishu"
+##last = "R"
+##
+##full = first + " " + last
+##
+##print(full)
+
+##
+##Practice questions :
+
+    
+##Question 1
+##
+##Create a string variable called:
+##
+name="college"
+print(name)
+
+##
+##Question 2
+##
+##Print the first character of this string:
+
+city = "Hosur"
+print(city[0])
+
+
+##Question 3
+##Convert this string into uppercase:
+##
+name = "ishu"
+print(name.upper())
+
+##Question 4
+##Find the length of this string:
+
+language = "Python"
+print(len(language))
+
+##Question 5
+##Take user input for name and print:
+##
+##Hello <name>
+
+name=input("enter your name:")
+print("Hello" , name)
+
+
+##Question 6
+##Combine two strings:
+
+##first_name
+##last_name
+##and print full name.
+
+first_name="Ishwarya"
+last_name="Shetty"
+print(first_name + " " +last_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
