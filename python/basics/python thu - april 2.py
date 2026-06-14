04d8a7cfa840de10e9c5ef4fe0957ef99bb68f81
Ishwarya <moriyaishu2244@gmail.com>
2026-06-14 20:56:55 +0530

    Refactor Python script for clarity and consistency

--- python/basics/python thu - april 2.py
+++ python/basics/python thu - april 2.py
@@ -1,32 +1,22 @@
-#1Print all elements using index
+1Print all elements using index
 
 arr=[2,4,6,8]
 for i in range(len(arr)):
     print(i,arr[i])
 
-# Add element 10 to list and print
+Add element 10 to list and print
 
 
 arr=[2,4,6,8]
 arr.append(10)
 print(arr)
 
-#3.
+3.
 arr.remove(arr[-1])
 print(arr)
 
 
-#5.Count how many elements are greater than 5
-
-arr=[2,7,4,9,1]
-count =0
-for x in arr:
-    if x > 5:
-        count+=1
-print(count)
-
-
-#4. 👉 Find sum of all elements using index
+4. 👉 Find sum of all elements using index
 
 arr=[2,4,6,8]
 total=0
@@ -36,5 +26,13 @@ print(total)
 
 
 
+5.Count how many elements are greater than 5
+
+arr=[2,7,4,9,1]
+count =0
+for x in arr:
+    if x > 5:
+        count+=1
+print(count)
 
 
