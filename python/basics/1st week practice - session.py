d08566b2982a789064988088a611e3899f14cc33
Ishwarya <moriyaishu2244@gmail.com>
2026-06-14 20:54:57 +0530

    Fix formatting and update comments in session.py

--- python/basics/1st week practice - session.py
+++ python/basics/1st week practice - session.py
@@ -1,9 +1,9 @@
-##1. What is a variable ?
-##
-##variable is container to store a values . example : a=0, a is a variable
-##
-##2. Take a number from user and print:
-##
+1. What is a variable ?
+
+variable is container to store a values . example : a=0, a is a variable
+
+2. Take a number from user and print:
+
 a=int(input("enter a:"))
 if a>0:
     print("positive")
@@ -15,7 +15,7 @@ else:
     print("none")
 
 
-##Q3️⃣ Print numbers from 1 to 10, but skip 4 and 7
+Q3️⃣ Print numbers from 1 to 10, but skip 4 and 7
 
 for i in range(1,11):
     if i==4:
@@ -26,7 +26,7 @@ for i in range(1,11):
 
 
 
-#4.Print numbers from 5 to 1 using while
+4.Print numbers from 5 to 1 using while
 
 i=5
 while i >0:
@@ -34,9 +34,9 @@ while i >0:
     i-=1
 
 
-##Q5️⃣ Print numbers from 1 to 10
-##
-##👉 Stop when number is 6
+Q5️⃣ Print numbers from 1 to 10
+
+👉 Stop when number is 6
 
 
 for i in range(1,11):
@@ -46,7 +46,7 @@ for i in range(1,11):
 
 
 
-##Q6️⃣ Count how many numbers are odd
+Q6️⃣ Count how many numbers are odd
 
 
 arr=[2,5,7,8,9,10]
@@ -58,7 +58,7 @@ print(count)
 
 
 
-##Q6️⃣ Count how many numbers are odd
+Q6️⃣ Count how many numbers are odd
 
 
 for i in range(1,5):
