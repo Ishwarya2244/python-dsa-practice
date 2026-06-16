b746726ba6c8e9ddf36cae3a07ca8c8268f01420
Ishwarya <moriyaishu2244@gmail.com>
2026-06-16 20:55:30 +0530

    Create Day6_Reverse_List.py for reversing a list
    Added a Python script to reverse a list with examples.

---
+++ dsa/JUNE - 2026 DSA journey/Arrays_Lists/Day6_Reverse_List.py
@@ -0,0 +1,31 @@
+DSA DAY 6 - REVERSE A LIST
+
+Goal:
+Reverse the order of elements in a list.
+
+Example:
+
+numbers = [5, 10, 15, 20]
+
+numbers.reverse()
+
+print(numbers)
+
+Output:
+
+[20, 15, 10, 5]
+
+Important:
+
+reverse() reverses the list in place.
+
+Memory Trick:
+
+Original:
+[5, 10, 15, 20]
+
+Reversed:
+[20, 15, 10, 5]
+
+First element becomes last.
+Last element becomes first.
