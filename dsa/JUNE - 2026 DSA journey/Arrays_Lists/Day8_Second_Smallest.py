718c743d2ca47f4e647261cf751fe07443137a62
Ishwarya <moriyaishu2244@gmail.com>
2026-06-20 21:17:59 +0530

    Add script to find second smallest number in a list
    Implement a script to find the second smallest number in a list by sorting the list and accessing the second element.

---
+++ dsa/JUNE - 2026 DSA journey/Arrays_Lists/Day8_Second_Smallest.py
@@ -0,0 +1,26 @@
+DSA DAY 8 - SECOND SMALLEST NUMBER
+
+Goal:
+Find the second smallest number in a list.
+
+Example:
+
+numbers = [8, 25, 15, 30]
+
+numbers.sort()
+
+print(numbers[1])
+
+Output:
+
+15
+
+Important:
+
+After sorting:
+
+[8, 15, 25, 30]
+
+numbers[0] = Smallest Number
+
+numbers[1] = Second Smallest Number
